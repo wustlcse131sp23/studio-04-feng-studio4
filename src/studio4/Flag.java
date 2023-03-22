41ea2a15844314110f4b63fdc7ec1782baf20751
14086 <14086@LAPTOP-HBQR7FF5>
2023-03-21 21:09:28 -0500

    Studio 4 Kelly Feng

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,31 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		   StdDraw.clear();
+		   StdDraw.setPenColor(0, 0, 0);
+		   StdDraw.filledRectangle(1, 1, 1, 1);
+		   StdDraw.setPenColor(255, 153, 204);
+		   StdDraw.filledRectangle(0, 1, 1, 0.9);
+		   StdDraw.setPenColor(102, 0, 204);
+		   StdDraw.filledRectangle(0, 1, 1, 0.8);
+		   StdDraw.setPenColor(0, 0, 255);
+		   StdDraw.filledRectangle(0, 1, 1, 0.7);
+		   StdDraw.setPenColor(0, 204, 204);
+		   StdDraw.filledRectangle(0, 1, 1, 0.6);
+		   StdDraw.setPenColor(0, 255, 0);
+		   StdDraw.filledRectangle(0, 1, 1, 0.5);
+		   StdDraw.setPenColor(255, 255, 0);
+		   StdDraw.filledRectangle(0, 1, 1, 0.4);
+		   StdDraw.setPenColor(255, 128, 0);
+		   StdDraw.filledRectangle(0, 1, 1, 0.3);
+		   StdDraw.setPenColor(255, 0, 0);
+		   StdDraw.filledRectangle(0, 1, 1, 0.2);
+		   StdDraw.setPenColor(0, 0, 0);
+		   StdDraw.filledRectangle(0, 1, 1, 0.1);
+		   
+		   StdDraw.setPenColor(0, 0, 0);
+		   StdDraw.filledRectangle(0, 0, 0.1, 1);
+		   StdDraw.filledRectangle(1, 0, 0.1, 1);
+		   
 	}
 }
